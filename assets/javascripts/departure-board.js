d649972d9c54025fc8cbe046a6f35fe333069db9
HWS-GER <heinze@websolution.company>
2016-09-07 16:11:33 +0200

    children not next()

--- assets/javascripts/departure-board.js
+++ assets/javascripts/departure-board.js
@@ -194,8 +194,8 @@ $(document).ready(function(){
         var a = jQuery('.editor a').attr("href");
         var url = "http://checkpointforarts.com"+a;
         var atag = "<a href='"+url+"' title='' />";
-        jQuery(this).next().wrapAll(atag);
-        jQuery(this).next().flapper({width:10,align:'left'}).change();
+        jQuery(this).children().wrapAll(atag);
+        jQuery(this).children().flapper({width:10,align:'left'}).change();
     });
 
 
